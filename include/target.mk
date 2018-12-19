6ec2acc84ba6352be9efa628e4a661a88bb6d5cf
presisco <internight@sina.com>
2018-12-19 18:37:20 +0800

    adjust default packages

--- include/target.mk
+++ include/target.mk
@@ -13,11 +13,22 @@ __target_inc=1
 DEVICE_TYPE?=router
 
 # Default packages - the really basic set
-DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd
+DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd block-mount \
+kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw \
+kmod-nls-cp932 kmod-nls-cp936 kmod-nls-cp950 \
+kmod-fs-exfat kmod-fs-ext4 kmod-fs-f2fs kmod-fs-msdos kmod-fs-vfat kmod-fuse ntfs-3g-utils e2fsprogs f2fs-tools mkf2fs fdisk \
+kmod-mtd-rw block-mount lsblk kmod-usb-uhci kmod-usb-ohci kmod-usb-storage-extras kmod-scsi-core \
+automount autosamba \
+default-settings luci-ssl-openssl luci-theme-material luci-app-adbyby-plus luci-app-autoreboot \
+luci-app-filetransfer luci-app-ssr-plus luci-app-vsftpd luci-app-xlnetacc \
+luci-app-arpbind luci-app-vlmcsd luci-app-ramfree \
+luci-app-wrtbwmon luci-app-aria2 ariang
 # For nas targets
 DEFAULT_PACKAGES.nas:=block-mount fdisk lsblk mdadm
 # For router targets
-DEFAULT_PACKAGES.router:=dnsmasq iptables ip6tables ppp ppp-mod-pppoe firewall odhcpd odhcp6c
+DEFAULT_PACKAGES.router:=dnsmasq-full iptables ppp ppp-mod-pppoe firewall \
+kmod-tcp-bbr kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw \
+luci-app-sfe luci-app-sqm luci-app-upnp luci-app-wifischedule luci-app-ramfree luci-proto-relay
 DEFAULT_PACKAGES.bootloader:=
 
 ifneq ($(DUMP),)
