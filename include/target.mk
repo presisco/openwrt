d64282cdd379bedf6eda1c5557dae6f86353bab8
presisco <internight@sina.com>
2018-12-24 14:48:41 +0800

    adjust default packages

--- include/target.mk
+++ include/target.mk
@@ -22,15 +22,15 @@ automount autosamba \
 luci-ssl-openssl luci-theme-material luci-app-adbyby-plus luci-app-autoreboot \
 luci-app-filetransfer luci-app-ssr-plus luci-app-vsftpd luci-app-xlnetacc \
 luci-app-arpbind luci-app-vlmcsd luci-app-ramfree \
-luci-app-wrtbwmon luci-app-aria2 ariang
+luci-app-wrtbwmon luci-app-aria2
 # For nas targets
 DEFAULT_PACKAGES.nas:=fdisk lsblk mdadm automount autosamba luci-app-usb-printer 
 # For router targets
 DEFAULT_PACKAGES.router:=dnsmasq-full iptables ppp ppp-mod-pppoe firewall \
 kmod-ipt-offload kmod-tcp-bbr kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw iptables-mod-fullconenat \
 luci-app-sfe luci-app-flowoffload luci-app-sqm luci-app-upnp luci-app-wifischedule luci-app-ramfree luci-proto-relay \
-luci-app-wireguard luci-app-ipsec-vpnd luci-app-openvpn-server luci-app-pptp-server luci-app-zerotier \
-luci-app-mwan3helper luci-app-amule luci-app-accesscontrol luci-app-guest-wifi ddns-scripts_aliyun luci-app-usb-printer luci-app-ddns
+luci-app-wireguard luci-app-ipsec-vpnd luci-app-openvpn-server luci-app-pptp-server luci-app-zerotier luci-app-frpc luci-app-dnsforwarder \
+luci-app-mwan3 luci-app-mwan3helper luci-app-accesscontrol luci-app-guest-wifi ddns-scripts_aliyun luci-app-usb-printer luci-app-ddns
 
 DEFAULT_PACKAGES.bootloader:=
 
